b27ce806ceee670c639bb52ec05dada786fd14b3
its-kapil <42872990+its-kapil@users.noreply.github.com>
2024-10-11 17:11:14 +0530

    Update HelloWorldController.java

--- hello_world/src/main/java/com/example/hello_world/HelloWorldController.java
+++ hello_world/src/main/java/com/example/hello_world/HelloWorldController.java
@@ -3,11 +3,27 @@ package com.example.hello_world;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// @RestController
+// public class HelloWorldController {
+
+//     @GetMapping("/")
+//     public String home() {
+//         return "<h1><b>Welcome to Telus Digital !!</b></h1>";
+//     }
+// }
+
 @RestController
 public class HelloWorldController {
 
     @GetMapping("/")
     public String home() {
-        return "<h1><b>Welcome to Telus Digital !!</b></h1>";
+        String poem = "<p><i>\"The woods are lovely, dark and deep,<br>" +
+                      "But I have promises to keep,<br>" +
+                      "And miles to go before I sleep,<br>" +
+                      "And miles to go before I sleep.\"</i><br><br>" +
+                      "- Robert Frost</p>";
+
+        String welcomeMessage = "<h1><b>Welcome to Telus Digital !!</b></h1>";
+        return poem + welcomeMessage;
     }
 }
